412d299a6d2f1404d63a026355eea7ad9910e8b1
Lindsay-R <lrecknell@gmail.com>
2014-06-18 22:13:30 -0600

    working on it

--- array_hard.rb
+++ array_hard.rb
@@ -1,8 +1,23 @@
+
+
 #Add all nums in an array
 
 array = [1, 2, 3]
 
-array2 = array.inject{|memo,n| memo + n }
-puts array2.inspect
+answer1 = array.inject{|memo,n| memo + n }
+puts answer1
+
+
+
+#Is an array a subset of an array?
+
+tags = %w[cat foo bar tod]
+p tags & %w[bar git]
+
+
+tags2 = %w[1 2 3]
+p tags2 & %w[3 2]
+p tags2 & %w[2 4]
+
 
 
